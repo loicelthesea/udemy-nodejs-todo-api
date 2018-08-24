5bc553be0fadebbf95862101e43573327045f897
Loic El Thesea <loic.elthesea@outlook.com>
2018-08-24 21:32:17 +0200

    Add GET /todos:id

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {ObjectID} = require('mongodb');
 
 const mongoose = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -8,7 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// POST TODOS
+const send404 = res => res.status(404).send({});
+const send400 = (res, err) => res.status(400).send(err);
+
+// POST A TODO
 app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text,
@@ -25,6 +29,21 @@ app.get('/todos', (req, res) => {
   );
 });
 
+// GET A TODO BY ID
+app.get('/todos/:id', (req, res) => {
+  const {id} = req.params;
+
+  if (!ObjectID.isValid(id)) {
+    send404(res);
+  } else {
+    Todo.findById(id).then(
+      todo => (todo ? res.send({todo}) : send404(res)),
+      error => send404(res),
+    );
+  }
+});
+
+// Start listening
 app.listen(3000, () => console.log('Todo API is up on port 3000'));
 
 module.exports = {app};
